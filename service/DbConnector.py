ae20c8822badb91c4c6de3e5ce231af734522e0f
Hafsa <hafsa390@gmail.com>
2023-03-28 00:25:26 -0400

    updated

--- service/DbConnector.py
+++ service/DbConnector.py
@@ -22,7 +22,7 @@ try:
         pool_name="hma_pool",
         pool_size=20,
         pool_reset_session=True,
-        host="127.0.0.1",
+        host="mysql://b4f104fae29631:3719ff63@us-cdbr-east-06.cleardb.net/heroku_5a380db5178d389?reconnect=true",
         port=3306,
         user="b4f104fae29631",
         password="3719ff63",
